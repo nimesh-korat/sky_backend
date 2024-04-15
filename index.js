f4d7caa8539fc632b1fe51bb3c98352087d60381
nimesh-korat <nimeshkorat611@gmail.com>
2024-04-15 10:45:23 +0530

    session

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: true,
+      secure: false,
       sameSite: 'none',
       // domain: '.nimesh.engineer',
       maxAge: 1000 * 60 * 60 * 24 //oneDay
